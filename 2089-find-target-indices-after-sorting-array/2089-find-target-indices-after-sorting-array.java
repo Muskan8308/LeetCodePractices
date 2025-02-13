dc61fc252a04283603e10348252a12417b98d150
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-02-13 23:08:20 +0530

    Time: 3 ms (41.94%), Space: 43.2 MB (62.55%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-
+    
         // Sorting + Linear solution
 
         if(nums == null || nums.length == 0) return List.of();     
@@ -12,31 +12,6 @@ class Solution {
             if(nums[i] == target)
             list.add(i);
         }
-
         return list;
-
-    /*
-        int s = 0, e = nums.length-1;
-        List<Integer> list = new ArrayList<>();
-        if(nums == null || nums.length == 0) return List.of();      // will return an empty list
-
-        Arrays.sort(nums);
-        while(s <= e)
-        {
-            int mid = s + (e-s)/2;
-
-            if(target == nums[mid])
-            {
-                list.add(mid);
-                e = mid - 1;
-            }
-            else if(target > nums[mid])
-                s++;
-            else
-                e--;
-        }
-        Collections.sort(list);
-        return list;
-    */
     }
 }
