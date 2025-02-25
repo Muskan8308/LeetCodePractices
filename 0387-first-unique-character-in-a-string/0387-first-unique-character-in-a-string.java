394e582f03615e3926017ae0157e9cd424c0fb91
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-02-25 22:15:11 +0530

    Time: 5 ms (94.08%), Space: 45.1 MB (80.88%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,5 +1,20 @@
 class Solution {
     public int firstUniqChar(String s) {
+
+        int[] freq = new int[26];
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            freq[s.charAt(i)-'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(freq[s.charAt(i)-'a'] == 1)
+            return i;
+        }
+        return -1;
+    /*    
         for(int i = 0; i < s.length(); i++)
         {   
             boolean flag = true;
@@ -16,5 +31,6 @@ class Solution {
         }
         
         return -1;
+    */
     }
 }
