e7330f262c6d8b045032cfa16799766cf47a8458
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-04-02 14:23:19 +0530

    Time: 2 ms (46.23%), Space: 43.8 MB (7.6%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,5 +1,13 @@
 class Solution {
     public int findGCD(int[] nums) {
+
+        // Recursive approach - using euclid's algorithm
+
+        Arrays.sort(nums);
+        return gcd(nums[0], nums[nums.length-1]);
+
+        /* 
+        // Iterative APproach
         int maxi = Integer.MIN_VALUE, mini = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i++)
         {
@@ -15,5 +23,14 @@ class Solution {
             mini = ans;
         }
         return mini;
+        */
+    }
+
+    private int gcd(int m, int n)
+    {
+        if (n == 0)
+        return m;
+
+        return gcd(n, m % n);
     }
 }
