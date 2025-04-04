c11aeda052d4aaeb9217cbc005286736c71e21ab
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-04-04 22:43:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (46.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,9 +1,9 @@
 class Solution {
     public double myPow(double x, int n) {
         
+        // making power positive
         long N = n;
-
-        if(N < 0)   //  For negative n
+        if(N < 0)
         {
             x = 1 / x;
             N = -N;
@@ -11,11 +11,13 @@ class Solution {
         return power(x, N);
     }
 
-    double power(double x, long n)
+    double power(double x, long N)
     {
-        if(n == 0)  return 1;
-        double ans = power(x, n/2);
-        if(n % 2 == 0)
+        if(N == 0)
+        return 1;
+
+        double ans = power(x, N/2);
+        if(N % 2 == 0)
         return ans * ans;
         else
         return ans * ans * x;
