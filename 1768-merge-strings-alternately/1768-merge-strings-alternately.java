161b5956dc6039c1e7630aa52f2728ecf5865666
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-04-07 22:34:43 +0530

    Time: 0 ms (100%), Space: 42 MB (36.07%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0, j = 0;
+        int n = word1.length(), m = word2.length(); 
+        StringBuilder str = new StringBuilder();
+        while(i < n && j < m)
+        {
+            str.append(word1.charAt(i));
+            str.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+
+        while(i < n)
+        {
+            str.append(word1.charAt(i));
+            i++;
+        }
+        while(j < m)
+        {
+            str.append(word2.charAt(j));
+            j++;
+        }
+
+        return str.toString();
+    }
+}
