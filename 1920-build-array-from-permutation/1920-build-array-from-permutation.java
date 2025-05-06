708010d094df3d5d56fb8fd45d38fd7a28888746
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-06 16:54:06 +0530

    1920-build-array-from-permutation

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,6 +1,24 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        
+
+        int n = nums.length;
+
+        // first pass - this will give the original value
+        for(int i = 0; i < n; i++)
+        {
+            nums[i] = nums[i] + (n * (nums[nums[i]] % n));
+        }
+
+        // second pass - this will give the new value
+        for(int i = 0; i < n; i++)
+        {
+            nums[i] = nums[i]/n;
+        }
+
+        return nums;
+
+     /*   
+        BURTE FORCE APPROACH 
         int n = nums.length;
         int j = 0;
         int[] ans = new int[n];
@@ -11,5 +29,6 @@ class Solution {
             j++;
         }
         return ans;
+     */
     }
 }
