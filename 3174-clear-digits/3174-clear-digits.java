9a76fc930e7369e99c68a4e776d959893d8b214c
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-07 21:52:57 +0530

    3174-clear-digits

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -4,11 +4,9 @@ class Solution {
         for(int i = 0; i < s.length(); i++)
         {
             char ch = s.charAt(i);
-            if(!st.isEmpty() && (st.peek() >= 97 && st.peek() <= 122) && (ch  >= 48 && ch <= 57))
-            {
-                st.pop();
-                continue;
-            }
+            if(!st.isEmpty() && (ch  >= 48 && ch <= 57))
+            st.pop();
+            else
             st.push(ch);
         }
 
