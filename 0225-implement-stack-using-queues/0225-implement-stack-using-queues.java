7e212da2ee8be574a598bc10814bb5f22f7c46c7
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-10 17:35:13 +0530

    0225-implement-stack-using-queues

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -5,26 +5,24 @@ class MyStack {
     public MyStack() {
         
     }
-    
-    public void push(int x) {
-        q.add(x);
+    // Pop-Efficient code
+    public void push(int x) {        // O(n)
+        if(q.size() == 0)
+            q.add(x);
+        else
+        {
+            q.add(x);
+            for(int i = 0; i < q.size()-1; i++)
+            q.add(q.remove());
+        }
     }
     
-    public int pop() {
-        for(int i = 0; i < q.size()-1; i++)
-        q.add(q.remove());
-
-        int val = q.remove();
-        return val;
+    public int pop() {      // O(1)
+        return q.remove();
     }
     
-    public int top() {
-        for(int i = 0; i < q.size()-1; i++)
-        q.add(q.remove());
-
-        int val = q.element();
-        q.add(q.remove());
-        return val;
+    public int top() {       // O(1)
+       return q.peek();
     }
     
     public boolean empty() {
