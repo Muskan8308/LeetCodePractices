324d5c7c68658199fefebdcc40bcaf276ef1461b
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-12 22:32:34 +0530

    1550-three-consecutive-odds

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,6 +1,21 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        
+
+        int cnt = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] % 2 == 1)
+            cnt++;
+            else 
+            cnt = 0;
+
+            if(cnt == 3)
+            return true;
+        }
+
+        return false;
+
+       /* 
         Stack<Integer> st = new Stack<>();
 
         for(int i = 0; i < arr.length; i++)
@@ -17,5 +32,7 @@ class Solution {
             return true;
         }
         return false;
+
+        */
     }
 }
