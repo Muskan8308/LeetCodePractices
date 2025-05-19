be4ecf7684c3b47799411d5d7ddff1fa02d0e1af
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-19 17:04:24 +0530

    2744-find-maximum-number-of-string-pairs

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
@@ -5,12 +5,8 @@ class Solution {
         int cnt = 0;
         for(int i = 0; i < words.length; i++)
         {
-            String rev = reverse(words[i]);
-            if(set.contains(rev))
-            {
-                cnt++;
-                set.remove(rev);
-            }
+            if(set.contains(reverse(words[i])))
+            cnt++;
             else
             set.add(words[i]);
         }
