839223647d387d8911eb630ecf19786305b01a96
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-23 20:34:21 +0530

    0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if(s.length() != t.length())
-        return false; 
         
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
+        if(s.length() != t.length())
+        return false;
 
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
 
-        return Arrays.equals(sArr,tArr);
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(map1.containsKey(ch))         // update the freq
+            map1.put(ch, map1.get(ch) + 1);
+            else                            // insert the new one
+            map1.put(ch, 1);
+        }
+
+        for(int i = 0; i < t.length(); i++)
+        {
+            char ch = t.charAt(i);
+            if(map2.containsKey(ch))         // update the freq
+            map2.put(ch, map2.get(ch) + 1);
+            else                            // insert the new one
+            map2.put(ch, 1);
+        }
+
+        return map1.equals(map2);
     }
 }
