171fdea1f39ea0d508a3bfd07fb08a336a477003
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-24 16:56:54 +0530

    0205-isomorphic-strings

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -10,11 +10,11 @@ class Solution {
 
             if(map.containsKey(sch))
             {
-                if(map.get(sch) != tch) return false;
+                if(map.get(sch) != tch) return false;   // checks if curret charcter is mapped to two different characters
             }
-            else if(map.containsValue(tch))
+            else if(map.containsValue(tch))     // if the 't' string's character already mapped to 's' string's character
             return false;
-            else
+            else                // if not present, then simply insert in the map
             map.put(sch, tch);
         }
         return true;
