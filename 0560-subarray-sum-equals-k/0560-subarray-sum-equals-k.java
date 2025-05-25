13a01ba2001a483addda7a2eead5eb839e5d9665
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-05-25 11:17:46 +0530

    0560-subarray-sum-equals-k

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        int prefSum = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            prefSum += nums[i];
+            if(map.containsKey(prefSum - k))
+            {
+                count += map.get(prefSum-k);
+            }
+            map.put(prefSum, map.getOrDefault(prefSum, 0) + 1);
+        }
+        return count;
+    }
+}
