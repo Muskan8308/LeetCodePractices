27d88c9d8e5fdd27ad9517c57feabdf718b8b44b
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-06-02 10:14:00 +0530

    0036-valid-sudoku

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,40 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i = 0; i < 9; i++)
-        {
-            for(int j = 0; j < 9; j++)
-            {
-                if(board[i][j] == '.') continue;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
 
                 char num = board[i][j];
                 board[i][j] = '.';
-                if(!isValid(board, i, j, num))  return false;
-                board[i][j] = num;
+                if (!isValid(board, i, j, num))
+                    return false;
+                board[i][j] = num;          // backtrack
             }
         }
         return true;
     }
 
-    boolean isValid(char[][] board, int row, int col, char num)
-    {
+    boolean isValid(char[][] board, int row, int col, char num) {
         // check row
-        for(int j = 0; j < 9; j++)
-        {
-            if(board[row][j] == num)  return false;
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num)
+                return false;
         }
 
         // check col
-        for(int i = 0; i < 9; i++)
-        {
-            if(board[i][col] == num)  return false;
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num)
+                return false;
         }
 
         // check 3 x 3 grid
-        int sRow = row/3 * 3, sCol = col/3 * 3;
+        int sRow = row / 3 * 3, sCol = col / 3 * 3;
 
-        for(int i = sRow; i < sRow + 3; i++)
-        {
-            for(int j = sCol; j < sCol + 3; j++)
-            {
-                if(board[i][j] == num)  return false;
+        for (int i = sRow; i < sRow + 3; i++) {
+            for (int j = sCol; j < sCol + 3; j++) {
+                if (board[i][j] == num)
+                    return false;
             }
         }
 
