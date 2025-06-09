990c92237d49df63c1b670426d55640519b29340
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-06-09 11:03:51 +0530

    0236-lowest-common-ancestor-of-a-binary-tree

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(p == root || q == root) return root;
+        if(p == q) return p;
+        boolean leftP = contains(root.left, p);
+        boolean rightQ = contains(root.right, q);
+        if((leftP && rightQ) || (!leftP && !rightQ)) return root;
+        if(leftP && !rightQ) return lowestCommonAncestor(root.left, p, q);
+        if(!leftP && rightQ) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+    private boolean contains(TreeNode root, TreeNode node)
+    {
+        if(root == null) return false;
+        if(node == root) return true;
+        return contains(root.left, node) || contains(root.right, node);
+    }
+}
