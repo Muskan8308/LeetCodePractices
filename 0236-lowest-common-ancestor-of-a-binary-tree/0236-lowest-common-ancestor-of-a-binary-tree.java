10268ced32ad2064fbd472c7dd09278536a7be3b
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-06-11 22:34:20 +0530

    0236-lowest-common-ancestor-of-a-binary-tree

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,20 +10,10 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        if(p == root || q == root) return root;
-        if(p == q) return p;
-        boolean leftP = contains(root.left, p);
-        boolean rightQ = contains(root.right, q);
-        if((leftP && rightQ) || (!leftP && !rightQ)) return root;       // basically, both p and q are in subtrees therefore LCA will be the root
-        if(leftP && !rightQ) return lowestCommonAncestor(root.left, p, q);      // if q is not in right subtree then definitely it will be in left subtree
-        if(!leftP && rightQ) return lowestCommonAncestor(root.right, p, q);     // if p is not in left subtree then definitely it will be in right subtree
-        return root;
-    }
-
-    private boolean contains(TreeNode root, TreeNode node)
-    {
-        if(root == null) return false;
-        if(node == root) return true;
-        return contains(root.left, node) || contains(root.right, node);
+        if(root == null || root == p || root == q)  return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
     }
 }
