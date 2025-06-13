3bc0297a2139c7ed0064d6332ea6d868da2fc5c2
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-06-13 09:36:25 +0530

    0101-symmetric-tree

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public boolean isSame(TreeNode p, TreeNode q)
+    {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSame(p.left, q.left) && isSame(p.right, q.right);
+    }
+
+    public TreeNode invertTree(TreeNode root)
+    {
+        if(root == null) return root;
+        TreeNode l = root.left;
+        TreeNode r = root.right;
+        root.left = invertTree(r);
+        root.right = invertTree(l);
+
+        return root;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        root.left = invertTree(root.left);
+        return isSame(root.left, root.right);
+    }
+}
