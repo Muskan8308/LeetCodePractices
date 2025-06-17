2dec00d7b84a467f3bb8d0fc052b03b827cf54d2
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-06-17 10:58:42 +0530

    0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,27 +4,32 @@ class Solution {
         if(s.length() != t.length())
         return false;
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for(int i = 0; i < s.length(); i++)
         {
             char ch = s.charAt(i);
-            if(map1.containsKey(ch))         // update the freq
-            map1.put(ch, map1.get(ch) + 1);
+            if(map.containsKey(ch))         // update the freq
+            map.put(ch, map.get(ch) + 1);
             else                            // insert the new one
-            map1.put(ch, 1);
+            map.put(ch, 1);
         }
 
         for(int i = 0; i < t.length(); i++)
         {
             char ch = t.charAt(i);
-            if(map2.containsKey(ch))         // update the freq
-            map2.put(ch, map2.get(ch) + 1);
-            else                            // insert the new one
-            map2.put(ch, 1);
+            if(!map.containsKey(ch))         
+            return false;
+           
+            map.put(ch, map.get(ch) - 1);
         }
 
-        return map1.equals(map2);
+        // All values in map should be 0 for s & t to be anagram
+        for(Integer i : map.values())
+        {
+            if(i != 0) return false;
+        }
+
+        return true;
     }
 }
