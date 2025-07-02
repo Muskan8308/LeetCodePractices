5ddc055fd593817cba348997d3d933e1d44b4ca1
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-07-02 23:03:43 +0530

    0101-symmetric-tree

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,28 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    // It includes the logic of SAME TREE + INVERT TREE = SYMMETRIC TREE 
-    public boolean isSame(TreeNode p, TreeNode q)
-    {
-        if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
-        if(p.val != q.val) return false;
-        return isSame(p.left, q.left) && isSame(p.right, q.right);
-    }
-
-    public TreeNode invertTree(TreeNode root)
-    {
-        if(root == null) return root;
-        TreeNode l = root.left;
-        TreeNode r = root.right;
-        root.left = invertTree(r);
-        root.right = invertTree(l);
-
-        return root;
-    }
-
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        root.left = invertTree(root.left);
         return isSame(root.left, root.right);
+    }
+
+    boolean isSame(TreeNode p, TreeNode q)
+    {
+        if(p == null && q == null) return true;
+        if(p == null || q == null || p.val != q.val) return false;
+        return isSame(p.left, q.right) && isSame(p.right, q.left);
     }
 }
