c2e27fe3b3f2c7e4420a6212cca656cc7e5a5299
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-07-11 16:12:37 +0530

    0538-convert-bst-to-greater-tree

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -21,13 +21,14 @@ class Solution {
         return root;
     }
 
+    // Reverse inorder traversal - Right Root Left
     public void inorderRev(TreeNode root, int[] sum)
     {
         if(root == null) return;
 
-        inorderRev(root.right, sum);
-        root.val = root.val + sum[0];
+        inorderRev(root.right, sum);        // right call
+        root.val = root.val + sum[0];       // self work
         sum[0] = root.val;
-        inorderRev(root.left, sum);
+        inorderRev(root.left, sum);         // left call
     }
 }
