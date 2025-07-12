c18254e06a71e77d736e3dfb3b54be4ef2c3e89c
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-07-12 23:01:47 +0530

    0094-binary-tree-inorder-traversal

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,6 +15,7 @@
  */
 class Solution {
     List<Integer> list = new ArrayList<>();
+    
     public List<Integer> inorderTraversal(TreeNode root) {
         
         if(root == null)    return list;
