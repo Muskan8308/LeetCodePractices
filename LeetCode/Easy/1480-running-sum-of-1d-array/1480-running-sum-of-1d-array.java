815587093cf53a824393096c352cb1ecb5dbd04c
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-08-22 22:18:50 +0530

    1480-running-sum-of-1d-array 08-22-2025

--- LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
 
-        int[] prefixSum = new int[nums.length];
-
-        prefixSum[0] = nums[0];
+        // inplace algo
         for(int i = 1; i < nums.length; i++)
         {
-            prefixSum[i] = nums[i] + prefixSum[i-1];
+            nums[i] = nums[i] + nums[i-1];
         } 
-        return prefixSum;
+        return nums;
     }
 }
