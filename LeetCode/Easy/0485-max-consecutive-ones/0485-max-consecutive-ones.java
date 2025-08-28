d2f3efbe24f64175794a47b55968c6988fee6815
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-08-28 15:11:08 +0530

    0485-max-consecutive-ones 08-28-2025

--- LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,6 +1,28 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        
+      
+        int len = 0, maxLen = 0, n = nums.length;
+        int i = 0, j = 0;
+        while(j < n)
+        {
+            if(nums[j] == 1)
+            {
+                len = j-i+1;
+                maxLen = Math.max(maxLen, len); 
+            }
+
+            while(j < n && nums[j] == 0)
+            {
+                j++;
+                i = j;
+            }
+            j++;
+        }
+
+        return maxLen;
+      
+      /*  
+        // Brute Force Approach
         int cnt = 0, maxCnt = 0;
 
         for(int i = 0; i < nums.length; i++)
@@ -14,5 +36,6 @@ class Solution {
             cnt = 0;
         }
         return maxCnt;
+       */ 
     }
 }
