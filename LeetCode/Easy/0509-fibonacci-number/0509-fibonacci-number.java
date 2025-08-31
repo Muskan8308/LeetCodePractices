64c4de842239f4f182f3aeac4177b23a35046332
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-08-31 19:40:00 +0530

    0509-fibonacci-number 08-31-2025

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,17 @@
 class Solution {
 
-    static int[] dp;
     public int fib(int n) {
-        
-        dp = new int[n+1];
-        return fibo(n);
+        int[] dp = new int[n+1];
+        return fibo(n, dp);
     }
 
-    private int fibo(int n)
+    private int fibo(int n, int[] dp)
     {
         if(n <= 1) return n;
 
-        if(dp[n] != 0) return dp[n];
+        if(dp[n] != 0) return dp[n];        // if val is already calculated then return the same, don't make further calls
         int ans = fibo(n-1) + fibo(n-2);
-        dp[n] = ans;
+        dp[n] = ans;                // Before return, store that value/answer in the array
         return ans;
     }
 }
