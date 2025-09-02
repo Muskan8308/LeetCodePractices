1b80d508ebc15c9c878e9bd9ffa2c1c4c905eac2
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-02 11:52:13 +0530

    0070-climbing-stairs 09-02-2025

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,20 @@
 class Solution {
+
+    // Tabulation 
+    public int climbStairs(int n)
+    {
+        int[] dp = new int[n+1];
+        dp[1] = 1; 
+        if(n > 1) dp[2] = 2;
+
+        for(int i = 3; i <= n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+
+        return dp[n]; 
+    }
+
+    /*
+     ------- Memoization -------
     public int climbStairs(int n) {
         
         int[] dp = new int[n+1];
@@ -12,4 +28,5 @@ class Solution {
         if(dp[n] != -1) return dp[n];
         return dp[n] = climbs(n-1, dp) + climbs(n-2, dp);
     }
+    */
 }
