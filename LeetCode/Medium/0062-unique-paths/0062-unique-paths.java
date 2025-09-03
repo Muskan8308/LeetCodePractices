80d322be55c8f3cebba0c3624d2aa63c62f64bc7
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-03 12:24:35 +0530

    0062-unique-paths 09-03-2025

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,20 @@
 class Solution {
+    
     public int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp)
+        Arrays.fill(arr, -1);
+
+        return paths(0, 0, m, n, dp);   
+    }
+
+    public int paths(int i, int j, int m, int n, int[][] dp) {
         
-        if(m == 1 || n == 1)    return 1;
+        if(i >= m || j >= n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int rightWays = uniquePaths(m, n-1);
-        int downWays = uniquePaths(m-1, n);
-
-        return rightWays + downWays;
+        return dp[i][j] = paths(i, j+1, m, n, dp) + paths(i+1, j, m, n, dp);
     }
 }
