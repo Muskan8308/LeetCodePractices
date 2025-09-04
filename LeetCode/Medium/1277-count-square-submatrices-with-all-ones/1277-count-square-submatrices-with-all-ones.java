f80ab032b77b71de69dacad53bb3f35edb1f7790
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-04 13:51:11 +0530

    1277-count-square-submatrices-with-all-ones 09-04-2025

---
+++ LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        
+        int m = matrix.length, n = matrix[0].length;
+        int cnt = 0;
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(matrix[i][j] == 0) continue;
+                else if(i > 0 && j > 0)
+                {
+                    matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i][j-1], matrix[i-1][j-1]));
+                }
+                cnt += matrix[i][j];
+            }
+        }
+        return cnt;
+    }
+}
