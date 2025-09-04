06271b481801759043a6abbf1da917852a83ae78
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-04 13:56:22 +0530

    1277-count-square-submatrices-with-all-ones 09-04-2025

--- LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -9,7 +9,7 @@ class Solution {
             for(int j = 0; j < n; j++)
             {
                 if(matrix[i][j] == 0) continue;
-                else if(i > 0 && j > 0)
+                if(i > 0 && j > 0)
                 {
                     matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i][j-1], matrix[i-1][j-1]));
                 }
