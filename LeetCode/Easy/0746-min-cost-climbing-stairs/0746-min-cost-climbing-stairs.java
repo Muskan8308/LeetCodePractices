92c955681698685db11705330a411fa0afe8c5e9
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-05 15:41:20 +0530

    0746-min-cost-climbing-stairs 09-05-2025

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
+        // Tabulation 
         int n = cost.length;
-        int[] dp = new int[n];
-        dp[n-1] = cost[n-1]; 
-        dp[n-2] = cost[n-2];
+        // int[] dp = new int[3];
+        int next1 = cost[n-1]; 
+        int next2 = cost[n-2];
 
         for(int i = n-3; i >= 0; i--)
         {
-            dp[i] = cost[i] + Math.min(dp[i+1], dp[i+2]);
+            int curr = cost[i] + Math.min(next1, next2);
+            next1 = next2;
+            next2 = curr;
         }
 
-        return Math.min(dp[0], dp[1]);
+        return Math.min(next1, next2);
     }
 }
