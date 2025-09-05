3f9407a6eb90fe2d072d8ad43e304e1df6e8807a
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-05 15:17:40 +0530

    0509-fibonacci-number 09-05-2025

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,13 +3,15 @@ class Solution {
         
         // Iterative DP - Tabulation
         if(n <= 1) return n;
-        int[] dp = new int[n+1];
+        int[] dp = new int[3];      // Space Optimization
 
         dp[0] = 0; dp[1] = 1;
         for(int i = 2; i <= n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2]; 
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2]; 
         }
-        return dp[n];
+        return dp[2];
     }
 }
