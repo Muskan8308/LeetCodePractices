d544c44cdf431a8c23218fb729207709b49e8159
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-06 23:17:56 +0530

    0121-best-time-to-buy-and-sell-stock 09-06-2025

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices) {
+
+
+        // OPTIMAL SOLUTION
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices)
+        {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+
+    /*    
+        // This is TABULATION T.C = O(n) and SC = O(n), Top-down DP giving TLE T.C = O(n^2) in worst case and SC = O(n).
+        
+        // This is just an another approach to solve this question. However, it is not an efficient solution for this question to give in an interview.
+        // Also, this solution is inspired by the optimal solution or we can say that we can optimize this solution by reducing its SC upto O(1) by using only 2 variables  instead of an dp array.
+
+
         int n = prices.length;
         int[] dp = new int[n+1];
         dp[0] = 0;
@@ -12,7 +34,8 @@ class Solution {
         }
 
         return dp[n-1];
-
+     */
+    
     }
 
     
