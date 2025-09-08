9c28ada34f974ef26651aab4219ad6b8a737e81d
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-09 00:00:12 +0530

    0494-target-sum 09-09-2025

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,14 +1,25 @@
 class Solution {
+
+    static int sum;
+
     public int findTargetSumWays(int[] nums, int target) {
         
-        return expCnt(nums, target, 0);
+        sum  = 0;
+        for(int ele : nums) sum += ele;
+
+        int[][] dp = new int[nums.length][2*sum+1];
+        for(int[] arr : dp)
+        Arrays.fill(arr, -1);
+
+        return expCnt(nums, target, 0, 0, dp);
     }
 
-    public int expCnt(int[] nums, int key, int i) {
+    public int expCnt(int[] nums, int key,int res, int i, int[][] dp) {
 
-        if(i == nums.length) return key == 0 ? 1 : 0;
-        int takePlus = expCnt(nums, key - nums[i], i+1);
-        int takeMinus = expCnt(nums, key + nums[i], i+1);
-        return takePlus + takeMinus;
+        if(i == nums.length) return res == key ? 1 : 0;
+        if(dp[i][res+sum] != -1) return dp[i][res+sum];
+        int takePlus = expCnt(nums, key, res - nums[i], i+1, dp);
+        int takeMinus = expCnt(nums, key, res + nums[i], i+1, dp);
+        return dp[i][res+sum] = takePlus + takeMinus;
     }
 }
