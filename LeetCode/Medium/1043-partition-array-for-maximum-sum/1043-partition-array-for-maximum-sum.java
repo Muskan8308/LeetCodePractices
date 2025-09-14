a990fa6e04bcfb2412e2f25c742c42f93eb251db
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-14 20:40:22 +0530

    1043-partition-array-for-maximum-sum 09-14-2025

--- LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,4 +1,10 @@
 class Solution {
+
+    /*
+     * This question comes under the category of Partition DP whose thinking is almost same as unbounded knapsack category.
+     * We have to think - "har index se hum kitne elements ka partition lenge (1 se k tak)".
+     * Then har choice/partition me se max element nikalo, uski length ko max element se multiply kro phir next element ko recursion sambhal lega.
+    */
      
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
@@ -19,7 +25,7 @@ class Solution {
         for(int j = i; j < Math.min(i+k, arr.length); j++)
         {
             max = Math.max(max, arr[j]);
-            int len = j - i + 1;    
+            int len = j - i + 1;                // length of the partition
             int sum = max * len + maxSum(arr, k, j+1, dp);
             ans = Math.max(ans, sum);
         }
