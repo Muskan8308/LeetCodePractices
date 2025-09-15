75ebdf2627a5ede0e42298db1b3072a143566f4f
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-15 22:45:24 +0530

    0322-coin-change 09-15-2025

--- LeetCode/Medium/0322-coin-change/0322-coin-change.java
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.java
@@ -1,7 +1,8 @@
 class Solution {
     public long coinCount( int i, int[] coins, int amount, long[][] dp) {
         
-        if(i == coins.length) 
+        // n-1 to 0
+        if(i == -1) 
         {
             if(amount == 0) return 0; 			// no count is needed
             else return Integer.MAX_VALUE;		// invalid count
@@ -9,7 +10,7 @@ class Solution {
         
         if(dp[i][amount] != -1) return dp[i][amount];
 
-        long skip = coinCount(i+1, coins, amount, dp);
+        long skip = coinCount(i-1, coins, amount, dp);
         if(amount - coins[i] < 0) return dp[i][amount] = skip;
         long take = 1 + coinCount(i, coins, amount - coins[i], dp);
         return dp[i][amount] = Math.min(skip, take);
@@ -23,7 +24,7 @@ class Solution {
     	for(long[] arr : dp)
     		Arrays.fill(arr, -1);
     	
-        int ans = (int)coinCount(0, coins, amount, dp);
+        int ans = (int)coinCount(coins.length-1, coins, amount, dp);
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
