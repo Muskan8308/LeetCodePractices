747b1d7c5648f876aeff68e921027348b54c2c4e
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-17 22:49:48 +0530

    2915-length-of-the-longest-subsequence-that-sums-to-target 09-17-2025

--- LeetCode/Medium/2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ LeetCode/Medium/2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,27 +1,27 @@
 class Solution {
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
         
-        // i -> n-1 to 0 | target/key -> target to 0
+        // i -> 0 to n-1 | target/key -> target to 0
         int[][] dp = new int[nums.size()][target+1];
         for(int[] arr : dp)
         Arrays.fill(arr, -1);
 
-        int ans = maxLength(nums, target, nums.size()-1, dp);
+        int ans = maxLength(nums, target, 0, dp);
         return ans < 0 ? -1 : ans;
     }
 
     public int maxLength(List<Integer> nums, int key, int i, int[][] dp) {
         
-        if(i == -1)
+        if(i == nums.size())
         {
             if(key == 0) return 0;          // A valid subsequence found
             else return Integer.MIN_VALUE;  // not found
         }
         if(dp[i][key] != -1) return dp[i][key];
 
-        int skip = maxLength(nums, key, i-1, dp);            // count laa ke dega
+        int skip = maxLength(nums, key, i+1, dp);            // count laa ke dega
         if(key - nums.get(i) < 0) return dp[i][key] = skip;
-        int take = 1 + maxLength(nums, key - nums.get(i), i-1, dp);
+        int take = 1 + maxLength(nums, key - nums.get(i), i+1, dp);
 
         return dp[i][key] = Math.max(skip, take); 
     }
