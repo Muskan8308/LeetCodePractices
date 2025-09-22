dcb774c7d582fb51aaea96e3af5f215dbe5d13bc
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-22 13:21:34 +0530

    0072-edit-distance 09-22-2025

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
@@ -3,25 +3,31 @@ class Solution {
 
         StringBuilder s = new StringBuilder(word1);
         StringBuilder t = new StringBuilder(word2);
-
         int m = s.length() , n = t.length();
-        return minSteps(s, t, m-1, n-1);
+
+        int[][] dp = new int[m][n];
+
+        for(int[] a : dp)
+        Arrays.fill(a, -1);
+
+        return minSteps(s, t, m-1, n-1, dp);
 
     }
 
-    public int minSteps(StringBuilder s, StringBuilder t, int i, int j) {
+    public int minSteps(StringBuilder s, StringBuilder t, int i, int j, int[][] dp) {
         
         if(i == -1) return j+1;
         if(j == -1) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(s.charAt(i) == t.charAt(j))
-        return minSteps(s, t, i-1, j-1);
+        return dp[i][j] = minSteps(s, t, i-1, j-1, dp);
         else 
         {
-            int del = minSteps(s, t, i-1, j);
-            int ins = minSteps(s, t, i, j-1);
-            int rep = minSteps(s, t, i-1, j-1);
-            return 1 + Math.min(del, Math.min(ins, rep));
+            int del = minSteps(s, t, i-1, j, dp);
+            int ins = minSteps(s, t, i, j-1, dp);
+            int rep = minSteps(s, t, i-1, j-1, dp);
+            return dp[i][j] = 1 + Math.min(del, Math.min(ins, rep));
         }
         
     }
