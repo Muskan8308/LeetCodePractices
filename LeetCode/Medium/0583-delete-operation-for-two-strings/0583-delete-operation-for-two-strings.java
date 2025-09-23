c677e139da4e043a46250d930a0eccd8194c2cd9
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-23 23:19:59 +0530

    0583-delete-operation-for-two-strings 09-23-2025

--- LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -2,17 +2,21 @@ class Solution {
     public int minDistance(String word1, String word2) {
         
         int m = word1.length(), n = word2.length();
-        int num = LCS(word1, word2, m-1, n-1);
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+
+        int num = LCS(word1, word2, m-1, n-1, dp);
 
         return ((m - num) + (n - num));
 
     }
 
-    public int LCS(String s, String r, int i, int j)
+    public int LCS(String s, String r, int i, int j, int[][] dp)
     {
         if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        if(s.charAt(i) == r.charAt(j)) return 1 + LCS(s, r, i-1, j-1);
-        else return Math.max(LCS(s, r, i, j-1), LCS(s, r, i-1, j));
+        if(s.charAt(i) == r.charAt(j)) return dp[i][j] = 1 + LCS(s, r, i-1, j-1, dp);
+        else return dp[i][j] = Math.max(LCS(s, r, i, j-1, dp), LCS(s, r, i-1, j, dp));
     }
 }
