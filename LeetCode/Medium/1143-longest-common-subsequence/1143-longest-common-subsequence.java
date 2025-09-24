073aa05cc0396629852aab10341182d7a13a3034
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-24 22:28:12 +0530

    1143-longest-common-subsequence 09-24-2025

--- LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,28 +2,25 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
         int m = text1.length(), n = text2.length();
-        StringBuilder s1 = new StringBuilder(text1);
-        StringBuilder s2 = new StringBuilder(text2);
+        char[] s = text1.toCharArray();
+        char[] t = text2.toCharArray();
 
-        int[][] dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
+        int[][] dp = new int[m+1][n+1];
 
-        return subSeq(s1, s2, m-1, n-1, dp);
-    }
-
-    public int subSeq(StringBuilder s1, StringBuilder s2, int i1, int i2, int[][] dp) 
-    {
-        if(i1 < 0 || i2 < 0) return 0;
-        if(dp[i1][i2] != -1) return dp[i1][i2];
-
-        if(s1.charAt(i1) == s2.charAt(i2))          // last char is same
-        return dp[i1][i2] = 1 + subSeq(s1, s2, i1 - 1, i2 - 1, dp);   
-        else                                        // last char differs
+        for(int i = m-1; i >= 0; i--)
         {
-            int ans1 = subSeq(s1, s2, i1 - 1, i2, dp);
-            int ans2 = subSeq(s1, s2, i1, i2 - 1, dp);
-            return dp[i1][i2] = Math.max(ans1, ans2);
+            for(int j = n-1; j >= 0; j--)
+            {
+                if(s[i] == t[j])                            // last char is same
+                dp[i][j] = 1 + dp[i+1][j+1];   
+                else                                        // last char differs
+                dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+
+            }
         }
 
+        return dp[0][0];
     }
+
+    
 }
