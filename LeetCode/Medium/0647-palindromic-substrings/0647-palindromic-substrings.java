84fe3924804bd7818fb85747a4cc1cca4dea21ca
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-09-27 11:09:55 +0530

    0647-palindromic-substrings 09-27-2025

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,20 +1,41 @@
 class Solution {
-    int cnt = 0;
-
-    private void isPalindrome(String s, int l, int r)
-    {
-        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r))
-        {
-            cnt++; l--; r++; 
-        }
-    }
-
     public int countSubstrings(String s) {
-       
-        for(int i = 0; i < s.length(); i++)
+        
+        int n = s.length(), cnt = 0;
+        int[][] dp = new int[n][n];
+
+        for(int k = 0; k < n; k++)      // kitni bar loop chalega
         {
-            isPalindrome(s, i, i);
-            isPalindrome(s, i, i+1);
+            int i = 0, j = k;   
+            while(j < n)                // daigonal loop
+            {
+                if(i == j)              // 1 length's palindrome
+                {
+                    dp[i][j] = 1;
+                    cnt++;
+                }
+                else if (j == i+1)      // 2 length's palindrome
+                {
+                    if(s.charAt(i) == s.charAt(j))
+                    {
+                        dp[i][j] = 1;
+                        cnt++;
+                    }
+                }
+                else                    // 3 or more length's palindrome
+                {
+                    if(s.charAt(i) == s.charAt(j))
+                    {
+                        if(dp[i+1][j-1] == 1)
+                        {
+                            dp[i][j] = 1;
+                            cnt++;
+                        }
+                    }
+                }
+                i++;
+                j++;
+            }
         }
         return cnt;
     }
