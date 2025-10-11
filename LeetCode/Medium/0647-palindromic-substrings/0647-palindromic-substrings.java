a9f9474230df0e9ba6a3de46723490bd23f02408
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-10-11 14:31:47 +0530

    0647-palindromic-substrings 10-11-2025

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,17 +4,17 @@ class Solution {
         int n = s.length(), cnt = 0;
         int[][] dp = new int[n][n];
 
-        for(int k = 0; k < n; k++)      // kitni bar loop chalega
+        for(int k = 0; k < n; k++)      // How many times the loop will execute
         {
-            int i = 0, j = k;   
-            while(j < n)                // daigonal loop
+            int i = 0, j = k;
+            while(j < n)                // Diagonal loop 
             {
-                if(i == j)              // 1 length's palindrome
+                if(i == j)              // 1 length palindrome
                 {
                     dp[i][j] = 1;
                     cnt++;
                 }
-                else if (j == i+1)      // 2 length's palindrome
+                else if(i+1 == j)       // 2 length palindrome
                 {
                     if(s.charAt(i) == s.charAt(j))
                     {
@@ -22,11 +22,11 @@ class Solution {
                         cnt++;
                     }
                 }
-                else                    // 3 or more length's palindrome
+                else                    // 3 or more length palindrome
                 {
                     if(s.charAt(i) == s.charAt(j))
                     {
-                        if(dp[i+1][j-1] == 1)
+                        if(dp[i+1][j-1] == 1) 
                         {
                             dp[i][j] = 1;
                             cnt++;
