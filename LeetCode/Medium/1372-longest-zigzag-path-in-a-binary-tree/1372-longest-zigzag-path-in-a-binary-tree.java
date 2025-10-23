95eb8fb5925662fae0d788a669d30cd84930169c
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-10-23 16:03:33 +0530

    1372-longest-zigzag-path-in-a-binary-tree 10-23-2025

--- LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -14,29 +14,28 @@
  * }
  */
 class Solution {
-    static int maxlen;
     public int longestZigZag(TreeNode root) {
 
-        maxlen = 0;
-        zigZag(root, 0, 0);         // 0 - left
-        zigZag(root, 1, 0);         // 1 - right
+        int[] maxlen = {0};
+        zigZag(root, 0, 0, maxlen);         // 0 - left
+        zigZag(root, 1, 0, maxlen);         // 1 - right
 
-        return maxlen;
+        return maxlen[0];
     }
 
-    private void zigZag(TreeNode root, int dir, int currlen) {
+    private void zigZag(TreeNode root, int dir, int currlen, int[] maxlen) {
         if(root == null) return;
-        maxlen = Math.max(maxlen, currlen);
+        maxlen[0] = Math.max(maxlen[0], currlen);
 
         if(dir == 1)                               // parent's direction is right   
         {
-            zigZag(root.left, 0, currlen + 1);     // going on opposite dir of parents will increase currlen
-            zigZag(root.right, 1, 1);              // going on the same dir of parents will again start the currlen to 1
+            zigZag(root.left, 0, currlen + 1, maxlen);     // going on opposite dir of parents will increase currlen
+            zigZag(root.right, 1, 1, maxlen);              // going on the same dir of parents will again start the currlen to 1
         }
         else                                       // parent's dir is left
         {
-            zigZag(root.left, 0, 1);
-            zigZag(root.right, 1, currlen + 1);
+            zigZag(root.left, 0, 1, maxlen);
+            zigZag(root.right, 1, currlen + 1, maxlen);
         }
 
     }
