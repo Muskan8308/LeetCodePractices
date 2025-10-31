be3d8543f3a659c4543b08d22930be811657c543
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-10-31 23:00:55 +0530

    1043-partition-array-for-maximum-sum 10-31-2025

--- LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -5,7 +5,7 @@ class Solution {
      * We have to think - "har index se hum kitne elements ka partition lenge (1 se k tak)".
      * Then har choice/partition me se max element nikalo, uski length ko max element se multiply kro phir next element ko recursion sambhal lega.
     */
-     
+
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
         int[] dp = new int[n];
@@ -17,16 +17,17 @@ class Solution {
     public int maxSum(int[] arr, int k, int i, int[] dp) {
 
         // base case
-        if(i == arr.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        
+        if (i == arr.length)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+
         int max = Integer.MIN_VALUE, ans = 0;
 
-        for(int j = i; j < Math.min(i+k, arr.length); j++)
-        {
+        for (int j = i; j < Math.min(i + k, arr.length); j++) {
             max = Math.max(max, arr[j]);
-            int len = j - i + 1;                // length of the partition
-            int sum = max * len + maxSum(arr, k, j+1, dp);
+            int len = j - i + 1; // length of the partition
+            int sum = max * len + maxSum(arr, k, j + 1, dp);
             ans = Math.max(ans, sum);
         }
 
