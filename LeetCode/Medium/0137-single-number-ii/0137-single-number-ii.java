50f8c9f79957ff24a17dce47901eb1f137f6ad19
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-12-18 15:25:18 +0530

    0137-single-number-ii 12-18-2025

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
@@ -1,19 +1,45 @@
 class Solution {
-    public int singleNumber(int[] nums) {
 
-        int n = nums.length;
+    // Brute Force Approach with TC - O(n^2) and SC - O(1)
+    // public int singleNumber(int[] nums)
+    // {
 
-        int cnt = 1;
-        for(int i = 0; i < n; i++)
+    // }
+
+    // Brute Force Approach with TC - O(n^2) and SC - O(1)
+    // public int singleNumber(int[] nums)
+    // {
+
+    // }
+
+    // Sorting Approach with TC - O(nlogn) and SC - O(logn)
+    public int singleNumber(int[] nums)
+    {
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length-2; i += 3)
         {
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j) continue;
-                if(nums[i] == nums[j]) cnt++;
-            }
-            if(cnt == 1) return nums[i];
-            cnt = 1;
+            if(nums[i] != nums[i+1]) return nums[i];
         }
-        return 1;
+        return nums[nums.length-1];
     }
+
+    // Brute Force Approach with TC - O(n^2) and SC - O(1)
+    // public int singleNumber(int[] nums) {
+
+    //     int n = nums.length;
+
+    //     int cnt = 1;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         for(int j = 0; j < n; j++)
+    //         {
+    //             if(i == j) continue;
+    //             if(nums[i] == nums[j]) cnt++;
+    //         }
+    //         if(cnt == 1) return nums[i];
+    //         cnt = 1;
+    //     }
+    //     return 1;
+    // }
 }
