9bc01ead5c26b6f8b0fbd6427bea0e81d4ac8345
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-12-18 14:57:36 +0530

    0136-single-number 12-18-2025

--- LeetCode/Easy/0136-single-number/0136-single-number.java
+++ LeetCode/Easy/0136-single-number/0136-single-number.java
@@ -8,24 +8,33 @@ class Solution {
         
     // }
 
-    // public int singleNumber(int[] nums) {
-        
-    // }
-
-    // Brute Force approach
+    // Sorting Approach
     public int singleNumber(int[] nums) {
 
-        int n = nums.length;
-        for(int i = 0; i < n; i++)
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length-1; i += 2)
         {
-            boolean flag = false;
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j) continue;
-                if(nums[i] == nums[j]) flag = true;
-            }
-            if(!flag) return nums[i];
+            if(nums[i] != nums[i+1])
+            return nums[i];
         }
-        return 0;
+        return nums[nums.length-1];
     }
+
+    // Brute Force approach  TC - O(n^2)  SC - O(1)
+    // public int singleNumber(int[] nums) {
+
+    //     int n = nums.length;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         boolean flag = false;
+    //         for(int j = 0; j < n; j++)
+    //         {
+    //             if(i == j) continue;
+    //             if(nums[i] == nums[j]) flag = true;
+    //         }
+    //         if(!flag) return nums[i];
+    //     }
+    //     return 0;
+    // }
 }
