355198e19ca5e76a80c06dd66f45313cc9baa053
Muskan Chourasia <chourasiamuskan830@gmail.com>
2025-12-20 18:47:34 +0530

    2220-minimum-bit-flips-to-convert-number 12-20-2025

--- LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-
+       
         int xor = start ^ goal;
-        // now just count the set bits from the xor
-        
-        int set = 0;
+        int cnt = 0;        // Count the number of 1's in the binary representation of xor
         while(xor != 0)
         {
-            xor = xor & (xor-1);
-            set++;
+            cnt += xor & 1;      // Check if the last bit is 1
+            xor >>= 1;           // Shift right to check the next bit
         }
-        return set;        // no. of different bits
+        return cnt;
     }
 }
