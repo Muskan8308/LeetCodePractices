30784fec8761486b90651e696b87dba5eaa5a4ff
Muskan Chourasia <chourasiamuskan830@gmail.com>
2026-01-12 22:17:28 +0530

    0207-course-schedule 01-12-2026

---
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        int[] indeg = new int[n];
+
+        // convert the given array into adjacency list
+
+        for(int i = 0; i < n; i++)
+        adj.add(new ArrayList<>());
+
+        for(int i = 0; i < pre.length; i++)
+        {
+            int a = pre[i][0], b = pre[i][1];
+            adj.get(b).add(a);
+            indeg[a]++;
+        }
+        
+        // implementing Kahn's algorithm
+        Queue<Integer> q = new LinkedList<>();
+
+        // find the node which has 0 as indegree
+        for(int i = 0; i < n; i++)
+        {
+            if(indeg[i] == 0)
+            q.add(i);
+        }
+
+        while(q.size() > 0)
+        {
+            int front = q.remove();
+            ans.add(front);
+            for(int ele : adj.get(front))
+            {
+                indeg[ele]--;
+                if(indeg[ele] == 0)
+                {
+                    q.add(ele);
+                }
+            }
+        }
+
+        return ans.size() == n;
+    }
+}
