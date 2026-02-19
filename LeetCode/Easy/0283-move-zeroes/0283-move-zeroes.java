b2bf47f8e52664c9b814b98c4d171811ecdd6879
Muskan Chourasia <chourasiamuskan830@gmail.com>
2026-02-19 18:30:04 +0530

    0283-move-zeroes 02-19-2026

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -2,18 +2,24 @@ class Solution {
     public void moveZeroes(int[] nums) {
 
         int n = nums.length;
-
+        int noz = 0;
         for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < n-1; j++)
+            if(nums[i] == 0) noz++;
+        }
+
+        for(int i = 0; i < noz; i++)
+        {
+            for(int j = 0; j < n-1-i; j++)
             {
-                if(nums[j] == 0 && nums[j+1] != 0)
+                if(nums[j] == 0)
                 {
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
                 }
             }
+            
         }
         
     }
